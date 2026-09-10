d42107c9e7bb3ca19ad7a384534053f5a345c9ca
ARUN KUMAR <ponthapalliarun@gmail.com>
2026-09-10 20:30:22 +0530

    Format plugins array in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+
   base: '/ponthapalliarunkumar-arun-portfolio/',
 })
